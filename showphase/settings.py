0c16ccf3fd767d08b199e54ef03e58a2ab0a81a3
IndieD3v <tanmaymakode76@gmail.com>
2021-04-13 17:16:11 +0530

    new databse

--- showphase/settings.py
+++ showphase/settings.py
@@ -107,6 +107,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 AUTHENTICATION_BACKENDS = [
     
